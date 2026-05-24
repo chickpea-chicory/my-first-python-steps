46ec21cae6583d3d0f97c64070e54c902711bb84
chickpea <nairn036@gmail.com>
2026-05-24 19:27:43 +0100

    Add files via upload

---
+++ intitnitntin.py
@@ -0,0 +1,13 @@
+count = 1
+while count <= 5:
+    print(count)
+    count = count + 1 #loop
+print("Loop done!")
+
+y = input("is this 5?").strip()
+if y == "yes": #checks user responce lolollol
+   print("yay")
+else:
+    print("NOOOOO!")
+
+
